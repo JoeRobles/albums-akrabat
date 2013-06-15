1ba7cd691e8a5b27bd756ed53d63e792566bddad
Joe Robles <joe@colldev.com>
2013-06-14 23:17:05 -0500

    CRUD queries created

---
+++ application/models/DbTable/Albums.php
@@ -0,0 +1,67 @@
+<?php
+
+class Application_Model_DbTable_Albums extends Zend_Db_Table_Abstract {
+
+    protected $_name = 'albums';
+
+    /**
+     * Retrieves a single row as an array
+     * 
+     * @param integer $id
+     * @return array
+     * @throws Exception
+     */
+    public function getAlbum(integer $id)
+    {
+        $id = (int) $id;
+        $row = $this->fetchRow('id = ' . $id);
+        if (!$row) {
+            throw new Exception("Could not find row $id");
+        }
+        
+        return $row->toArray();
+    }
+
+    /**
+     * Creates a new row in the database
+     * 
+     * @param string $artist
+     * @param string $title
+     */
+    public function addAlbum(string $artist, string $title)
+    {
+        $data = array(
+            'artist' => $artist,
+            'title' => $title,
+        );
+        
+        $this->insert($data);
+    }
+
+    /**
+     * Updates an album row
+     * 
+     * @param integer $id
+     * @param string $artist
+     * @param string $title
+     */
+    public function updateAlbum(integer $id, string $artist, string $title)
+    {
+        $data = array(
+            'artist' => $artist,
+            'title' => $title,
+        );
+        
+        $this->update($data, 'id = ' . (int) $id);
+    }
+
+    /**
+     * Removes the row completely
+     * 
+     * @param integer $id
+     */
+    public function deleteAlbum(integer $id)
+    {
+        $this->delete('id =' . (int) $id);
+    }
+}
